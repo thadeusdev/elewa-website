413068ddd5e6e3c1a699e951cdfe2f35215e10f0
thadeus <thadeusnyariki94@gmail.com>
2023-09-01 09:48:33 +0300

    :package: feat: Created conv-hero-section component

---
+++ libs/features/pages/conversational-learning/src/lib/components/conv-hero-section/conv-hero-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-conv-hero-section',
+  templateUrl: './conv-hero-section.component.html',
+  styleUrls: ['./conv-hero-section.component.scss'],
+})
+export class ConvHeroSectionComponent {}
